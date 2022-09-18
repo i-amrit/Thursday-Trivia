5d8ff6b92d65feaf514687d61e56846db17eac54
Amritpal Singh <22807344+i-amrit@users.noreply.github.com>
2022-09-18 20:43:34 +0530

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/thursdaytrivia/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.thursdaytrivia.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.thursdaytrivia.test";
+  public static final String BUILD_TYPE = "debug";
+}
